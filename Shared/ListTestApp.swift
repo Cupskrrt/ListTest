c1771f3f22b1a624242e85667d4b9eb512fff921
Cupang5 <khanalfarizzi@hotmail.com>
2022-05-16 20:31:00 +0700

    Initial Commit

---
+++ Shared/ListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListTestApp.swift
+//  Shared
+//
+//  Created by Luthfi Khan on 16/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
